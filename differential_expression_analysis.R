5d2588c3b731a469a39d2f3db1b8e11fde333d1a
imnikhilanand <imnikhilanand@gmail.com>
2023-03-28 01:24:32 -0400

    Adding the differential expression analysis file

---
+++ differential_expression_analysis.R
@@ -0,0 +1,13 @@
+
+# DEseq2 will be used to perform the differential expression analysis
+
+# Install the latest version of DEseq2
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install("DESeq2", version = "3.16")
+
+
+
+
+
+
