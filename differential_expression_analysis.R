9c5c95f4bc615003568f9d007dec497a18392a76
imnikhilanand <imnikhilanand@gmail.com>
2023-03-28 02:43:06 -0400

    Added code to load libraries and read the file

--- differential_expression_analysis.R
+++ differential_expression_analysis.R
@@ -1,10 +1,28 @@
 
+# setting the correct working directory
+setwd('D:/Classes/Applied Statistics/Project')
+
 # DEseq2 will be used to perform the differential expression analysis
 
 # Install the latest version of DEseq2
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 BiocManager::install("DESeq2", version = "3.16")
+
+
+# loading the libraries
+library(DESeq2)
+
+# Reading the dataset
+rawCounts <- read.csv("./dataset/kaggle_dataset/fpkm_table_unnormalized.csv")
+head(rawCounts)
+
+# Reading the sample data
+sampleData <- read.csv("./dataset/kaggle_dataset/columns-samples.csv")
+head(sampleData)
+
+
+
 
 
 
